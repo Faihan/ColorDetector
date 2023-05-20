f7f4d2308b0d1a4f9a07f979fca2a77fb3a74f2f
Muaz Variava <muaz.who@gmail.com>
2023-05-20 21:29:54 +0400

    Finished making the detector

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ while True:
     image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(image, lower, upper)
 
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) !=0:
+        for contour in contours:
+            if cv2.contourArea(contour) > 500:
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(img, (x,y), (x + w, y + h), (0, 0, 255), 3)
+
     cv2.imshow("mask", mask)
     cv2.imshow("webcame", img)
 
