d327e49685b0c67e4e626a1ef64dbfecb5fd46e7
Muaz Variava <muaz.who@gmail.com>
2023-06-06 13:36:54 +0400

    Added Close funtionality

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ while True:
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    if len(contours) !=0:
+    if len(contours) != 0:
         for contour in contours:
             if cv2.contourArea(contour) > 500:
                 x, y, w, h = cv2.boundingRect(contour)
-                cv2.rectangle(img, (x,y), (x + w, y + h), (0, 0, 255), 3)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
     cv2.imshow("mask", mask)
-    cv2.imshow("webcame", img)
+    cv2.imshow("webcam", img)
 
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == 27:  # ASCII value for Escape key
+        break
+
+video.release()
+cv2.destroyAllWindows()
